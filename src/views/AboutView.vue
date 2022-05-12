0e4ee161e8dc0587c4e2605fca713409b9dbb0b6
yaya7257 <fish7257@gmail.com>
2022-05-12 18:24:34 +0800

    test

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <BootCard></BootCard>
+</template>
+
+<script>
+import BootCard from '@/components/BSCard.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    BootCard
+  }
+}
+</script>
